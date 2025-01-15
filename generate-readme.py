ef02b80171965b6a64ef1f417308b26e29afb9bc
Theodore Cooper <ccooperr2005@gmail.com>
2025-01-15 23:23:46 +0800

    add python script to generate readme

---
+++ generate-readme.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+folder_path = "src"
+output_file = "README.md"
+
+def get_title_from_filename(filename):
+    name = filename[:-4]
+    name = name.replace('-', ' ')
+    match = re.match(r"(\d+)\.(.+)", name)
+    if match:
+        number = match.group(1)
+        title = number + ". " + match.group(2)
+        return int(number), title
+    return None, None
+
+def main():
+    files = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".cpp"):
+            number, title = get_title_from_filename(filename)
+            if number is not None:
+                files.append((number, title, filename))
+    files.sort()
+    
+    with open(output_file, "w") as f:
+        f.write(f"# Leetcode Solutions\n\nC++ source code of solved LeetCode problems.\n\n## List\n\nThe list is sorted by problem number.\n\n<details>\n\n<summary>Expand Solution List</summary>\n\n")
+        for number, title, filename in files:
+            f.write(f"- [{title}](src/{filename})\n")
+        f.write(f"\n</details>\n")
+
+if __name__ == "__main__":
+    main()
